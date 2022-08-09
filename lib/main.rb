b2f06f2e005437c3d2fa897a90c1299ee0f54733
wallison <wallisongoncalves105ibc@gmail.com>
2022-08-08 23:15:43 -0400

    Ve que o código está bem mais legivel, evite sempre colocar if's aninhados, lembre-se quase todo "if elsif" pode ser quebrado em funções. não tenha medo de fazer funções e sempre que faze-las coloque nomes expressivos tipo a função 'render_game_over' -> você entende só pelo nome da função que ela irá renderizar o game_over

---
+++ lib/main.rb
@@ -0,0 +1,67 @@
+require_relative 'adivinhar_numero.rb'
+require_relative 'rendering.rb'
+
+class Main
+    attr_reader :computador
+
+    def initialize
+        @computador = AdivinharNumero.new
+    end
+
+    def run
+        init_game
+    end
+
+    private
+
+    def init_game
+        loop do
+            user_input = Rendering.render_menu
+
+            if user_input == Rendering::INPUT_OPTION::PLAY
+                computador.jogar_novamente
+                count = 0
+                gameplay(count)
+            elsif user_input == Rendering::INPUT_OPTION::GAME_INFO
+                puts "Você tem 10 tentativas para conseguir descobrir qual foi o numero sorteado pela computador."
+            elsif user_input == Rendering::INPUT_OPTION::EXIT
+                system('cls')
+                break
+            end
+        end
+    end
+
+    def gameplay(count)
+        loop do
+            player_input = render_play
+
+            computador.adivinhar(player_input)
+            count += 1
+            break if game_finalized?(count)
+        end
+    end
+
+    def render_play
+        system('cls')
+        Rendering.animacao(Rendering::INPUT_OPTION::PLAY)
+        puts 'Digite um Numero entre 1 a 30:'
+        gets.chomp.to_i
+    end
+
+    def game_finalized?(count)
+        return true if computador.venceu
+        game_over?(count)
+    end
+
+    def game_over?(count)
+        return false if count < 10
+
+        system('cls')
+        Rendering.render_game_over
+        system('pause')
+        true
+    end
+end
+
+
+
